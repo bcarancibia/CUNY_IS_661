18086b615505bdc695a99bf766881bfd9a9f9d60
bcarancibia <bcarancibia@gmail.com>
2015-03-15 10:21:08 -0400

    update

--- scripts/exploratory_data_analysis.R
+++ scripts/exploratory_data_analysis.R
@@ -66,3 +66,64 @@ length(moldova)
 length(timor)
 length(uganda)
 length(honduras)
+
+length(which(chad==" "|chad==""))/(269*82)
+length(which(civ==" "|civ==""))/(408*88)
+length(which(haiti==" "|haiti==""))/(2211*41)
+length(which(honduras==" "|honduras==""))/(1033*111)
+length(which(kosovo==" "|kosovo==""))/(2185*14)
+length(which(madagascar==" "|madagascar==""))/(1142*42)
+length(which(malawi==" "|malawi==""))/(773*35)
+length(which(moldova==" "|moldova==""))/(377*17)
+length(which(timor==" "|timor==""))/(956*82)
+length(which(uganda==" "|uganda==""))/(1123*131)
+
+sum(chad$Actual.Disbursements)
+sum(civ$Actual.Disbursements)
+sum(kosovo$Total.Actual.Disbursements)
+sum(madagascar$Total.Disbursements)
+sum(malawi$Total.Actual.Disbursements)
+sum(timor$Total.Actual.Disbursements)
+sum(moldova$Total.Actual.Commitments)
+
+
+#group by donor 
+#chad
+by_donor_chad <- group_by(chad, Donor.Agency)
+summary_donor_chad <- summarise(by_donor_chad, count_each_donor = n(),
+                                                total_by_donor = sum(Actual.Disbursements))
+
+#civ
+by_donor_civ <- group_by(civ, Donor.Agency)
+summary_donor_civ <- summarise(by_donor_civ, count_each_donor = n(),
+                                total_by_donor = sum(Actual.Disbursements))
+
+#kosovo
+by_donor_kosovo <- group_by(kosovo, Donor.Agency)
+summary_donor_kosovo <- summarise(by_donor_kosovo, count_each_donor = n(),
+                               total_by_donor = sum(Total.Actual.Disbursements))
+
+#madagascar
+by_donor_madagascar <- group_by(madagascar, AGENCE)
+summary_donor_madagascar <- summarise(by_donor_madagascar, count_each_donor = n(),
+                                  total_by_donor = sum(Total.Disbursements))
+
+#malawi
+by_donor_malawi <- group_by(malawi, Donor.Agency)
+summary_donor_malawi <- summarise(by_donor_malawi, count_each_donor = n(),
+                                      total_by_donor = sum(Total.Actual.Disbursements))
+
+#timor
+by_donor_timor <- group_by(timor, Donor.Agency)
+summary_donor_timor <- summarise(by_donor_timor, count_each_donor = n(),
+                                  total_by_donor = sum(Total.Actual.Disbursements))
+
+#moldova
+by_donor_moldova <- group_by(moldova, Donor)
+summary_donor_moldova <- summarise(by_donor_moldova, count_each_donor = n(),
+                                 total_by_donor = sum(Total.Actual.Commitments))
+
+
+
+
+
